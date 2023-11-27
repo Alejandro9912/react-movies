b8d106af3e802ef6e371206adfad911af098f2f2
Alejandro Ríos <juanalcarios@gmail.com>
2023-11-27 18:36:08 -0500

    Event Handlers

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,11 +2,15 @@ import MovieCard from "./MovieCard";
 
 const MovieList = (props) => {
   const { list } = props;
+
+  const handleCheckTime = (movieName) => {
+    console.log(`Check time clicked from ${movieName}`);
+  };
   return (
     <ul>
       {list.map((movie) => (
         // eslint-disable-next-line react/jsx-key
-        <MovieCard name={movie.name} />
+        <MovieCard name={movie.name} onCheckTimeClick={handleCheckTime} />
       ))}
     </ul>
   );
